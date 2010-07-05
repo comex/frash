7933cb76467ed89e09310f08fc3cd7a44963887f
Geoff Norton <plasma@limestone.local>
2010-07-05 18:10:06 -0400

    - Fix a few more sandbox issues - Sexify the click-to-run

--- Player2/Classes/PluginFlashView.h
+++ Player2/Classes/PluginFlashView.h
@@ -31,5 +31,6 @@
 - (void)displaySyncInRect:(CGRect *)rect;
 - (CGSize)movieSize;
 - (void)diedWithError:(NSString *)error;
+- (UIImage *)scaleImage:(UIImage *) image maxWidth:(float) maxWidth maxHeight:(float) maxHeight;
 
 @end
